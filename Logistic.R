27529b5100f3818e7016a9cfe56c2d21752a6585
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2022-03-29 23:09:26 -0600

    Update Logistic.R
    Small correctinos

--- Logistic.R
+++ Logistic.R
@@ -2,13 +2,13 @@
 # plot of the logistic function
 
 f_logistic <- function(x){
-  return(1 - exp(-x))^(-1)
+  return(1 + exp(-x))^(-1)
   }
   
 curve(f_logistic,
   from = -4, 
   to = 4,
-  ylav = "P",
+  ylab = "P",
   xlab = "X",
   main = "Logistic Function")
   
