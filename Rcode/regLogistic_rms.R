03e6be85702bdf54a9c1950e86624b1029cb9909
fabarrios <fbarrios@unam.mx>
2022-05-06 13:41:26 -0500

    Data new fron Daniel

--- Rcode/regLogistic_rms.R
+++ Rcode/regLogistic_rms.R
@@ -1,5 +1,8 @@
 # Logistic regression using rms
 # 
+library(rms)
+library(car)
+library(tidyverse)
 
 # Example from the WCGS data
 
@@ -11,11 +14,13 @@ wcgs_n <- wcgs %>% filter(chol < 645)
 
 datadist(wcgs_n)
 dd_CHDwcgs_n <- datadist(wcgs_n)
-options(datadist = 'dd_CHDwcgs_n')
+options(datadist = "dd_CHDwcgs_n", na.action = "na.delete")
 
 CHD_lrmMod <- lrm(chd69 ~ age + chol + bmi + sbp + smoke, data = wcgs_n)
 print(CHD_lrmMod)
 summary(CHD_lrmMod)
 
-# wcgs_n <- mutate(wcgs_n, PredCHD_lrm = Predict(CHD_lrmMod))
+wcgs_n <- mutate(wcgs_n, PredCHD_lrm = predict(CHD_lrmMod))
 
+ggplot(wcgs_n, aes(x = age, y = chd69, color = smoke)) +
+  geom_smooth(aes(y = PredCHD_lrm, fill = smoke), size = 0.75, alpha = .15)
