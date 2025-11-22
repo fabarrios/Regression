9763fe4e13aff8a18d968cec9f8a173576ed933a
[fbarrios@unam.mx] <fabarrios@users.noreply.github.com>
2025-11-22 13:48:54 -0600

    new boxplots
    Revised example of longitudinal data examples and new elongated data tables and boxplots

--- MLE/LinearMixed_exapls.R
+++ MLE/LinearMixed_exapls.R
@@ -11,20 +11,20 @@ BPRS <- BPRS %>% mutate(Group = factor(Group))
 BPRS_long <- BPRS %>% pivot_longer(cols = c("Week1", "Week2", "Week3", "Week4", "Week5", "Week6", "Week7", "Week8"), 
                                    names_to = "Week", values_to = "bprs")
 
-# To plot the data over the weeks including the Week0 and line per Subject
-BPRSL_new <- BPRS %>% pivot_longer(cols = c("Week0", "Week1", "Week2", "Week3", "Week4", "Week5", "Week6", "Week7", "Week8"),
+# Elongated BPRS to graph the data over the weeks including the Week0 and line per Subject
+BPRSL_graph <- BPRS %>% pivot_longer(cols = c("Week0", "Week1", "Week2", "Week3", "Week4", "Week5", "Week6", "Week7", "Week8"),
                                    names_to = "Week", values_to = "bprs")
 
-BPRSL_new %>% ggplot(aes(x = Week, y = bprs, color = Subject, group = Subject)) +
+BPRSL_graph %>% ggplot(aes(x = Week, y = bprs, color = Subject, group = Subject)) +
   geom_line() + scale_linetype_manual(values = rep(1:10, times = 4)) +
   facet_grid(. ~ Treatment, labeller = label_both) +
   theme(legend.position = "none") + theme_bw() +
   theme(panel.grid.minor.x = element_blank()) +
-  scale_y_continuous(limits = c(min(BPRSL_new$bprs), max(BPRSL_new$bprs)))
+  scale_y_continuous(limits = c(min(BPRSL_graph$bprs), max(BPRSL_graph$bprs)))
                                   
-BPRSL_new %>% ggplot(aes(x= Week, y= bprs, linetype = Subject, color = Subject)) +
-                     geom_boxplot(position = position_dodge(width = 0.9)) +
-                     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-                     theme(legend.position = c(0.8, 0.8)) + theme_bw() +
-                     labs(title = "BPRS data")
+BPRSL_graph %>% ggplot(aes(x= Week, y= bprs, fill = Treatment)) +
+          geom_boxplot(position = position_dodge(width = 0.8)) +
+          theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+          theme_bw() + theme(legend.position = c(0.9, 0.9)) +
+          labs(title = "Brief Psychiatric Rating Scale (BPRSL) data")
                      
